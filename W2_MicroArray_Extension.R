7ee8af26e2125153b303b8ec027f7133bd75c54d
SiWolf <silveranthony@hotmail.de>
2016-03-29 15:33:48 +0200

    Added files via upload
    Text-Datei-Erstellung von Yannic integriert, fehlende Skalierung bei mas5 ergänzt.

--- W2_MicroArray_Extension.R
+++ W2_MicroArray_Extension.R
@@ -2,7 +2,7 @@
 # File: W2_MicroArray_Extension.R
 # Title: SWP Programm Woche 2 - Einfuehrung in MicroArrays
 # By: Yannic Lapawczyk, Silver Wolf, Michael Krivab
-# Date: 27.03.2016
+# Date: 29.03.2016
 # Version: 1.0 (Praesentationsversion)
 # ---------------------------------------------------------
 
@@ -26,10 +26,10 @@ image(Data_All)
 
 # Normalisierung:
 Data_All_rma <- rma(Data_All)
-Data_All_mas5 <- mas5(Data_All)
+Data_All_mas5 <- mas5(Data_All, sc=150)
 
 Data_Ohne_Aussreisser_rma <- rma(Data_Ohne_Aussreisser)
-Data_Ohne_Aussreisser_mas5 <- mas5(Data_Ohne_Aussreisser)
+Data_Ohne_Aussreisser_mas5 <- mas5(Data_Ohne_Aussreisser, sc=150)
 
 # Histogramm-Erstellung:
 hist(Data_All)
@@ -56,6 +56,107 @@ heatmap(exprs(Data_All_mas5[1:100,]))
 
 heatmap(exprs(Data_Ohne_Aussreisser_rma[1:100,]))
 heatmap(exprs(Data_Ohne_Aussreisser_mas5[1:100,]))
+
+# Erstellen der beiden Normalisierungs-Tabellen:
+
+create_timestamp <- function() {
+  
+  # Timestamp erstellen fuer den Dateinamen
+  now <- Sys.time()
+  
+  # Doppelpunkte ersetzen
+  now <- gsub(":","-",now)
+  
+  # Luecke entfernen
+  now <- gsub(" ","-",now)
+  return(now)
+}
+
+# Funktion, die aus einem Datasatz ein Text-File erstellt
+# dataset: Datensatz (engelesene CEL-Files)
+# norm: Normalisierungsvariante (z.B. rma oder mas5)
+# headerBoolean: TRUE oder FALSE => Soll Text-File header haben oder nicht
+# info: Welche zusaetzliche Information mit geschrieben werden soll (z.b. GENENAME oder SYMBOL)
+print_to_file <- function(dataset, norm, headerBoolean, info) {
+  # Aktuelle Uhrzeit ablesen
+  timestamp <- create_timestamp()
+  
+  # Normalisierungsmethode und Info mit an Dateinamen haengen
+  filename <- paste(timestamp, norm, sep = "_")
+  filename <- paste(filename, info, sep = "_")
+  
+  # Dateiende anhaengen
+  filename <- paste(filename, ".txt", sep = "") 
+  
+  # Expressionset erstellen
+  if (norm == "rma") {
+    eset <- rma(dataset)
+  }
+  else if (norm == "mas5") {
+    eset <- mas5(dataset, sc = 150)
+  }
+  else{
+    print("Fehler: Keine zulaessige Normalisierung.")
+  }
+  
+  # Vektor erstellen, der alle 7 RMA/MAS5-Werte enthaelt
+  exprs_vector <- exprs(eset)
+  
+  # Alle AffyIDs aus Expressionset auslesen
+  affyids <- featureNames(eset)
+  
+  # Geninfo auslesen
+  geninfo <- select(hgu133plus2.db, affyids, info)
+  
+  # Falls nicht zu allen affyids die "info" gefunden wurde:
+  # Alle affyids durchgehen und schauen, ob ein Genname gefunden wurde
+  if(length(affyids) != nrow(geninfo)) {
+    print("Korrigiere Info-Vektor...")
+    
+    # Vektor mit Infos ueberarbeiten, damit auch affyids enthalten sind
+    # Fuer die keine Info gefunden wurde
+    # Leeren Vektor erstellen
+    modded_info <- vector(,nrow(exprs_vector))
+    
+    for(i in 1:length(affyids)) {
+      # Position von affyid in "geninfo" rausschreiben
+      position <- match(affyids[i],geninfo[,1])
+      # Wenn position != NA, dann ist die affyid in "geninfo"
+      # Und die Geninfo kann uebernommen werden
+      # Wenn fuer die affyid keine Info gefunden wurde,
+      # Wird ein "NotFound" eingefuegt
+      
+      if(!is.na(position)) {
+        modded_info[i] <- geninfo[position,2]
+      } else {
+        modded_info[i] <- "NotFound"
+      } 
+    }
+  }
+  # Wenn Laenge nicht unterschiedlich, dann einfach alle uebernehmen
+  else {
+    modded_info <- geninfo[,2]
+  }
+  
+  # Geninfo in Matrix einschieben
+  output <- cbind(affyids,modded_info,exprs_vector)
+  
+  # header erstellen
+  if(isTRUE(headerBoolean)) {
+    header <- append(c("AffyID",info),sampleNames(eset),2)
+    # header in Datei schreiben
+    write.table(t(header), file = filename, row.names = FALSE, col.names = FALSE, append = FALSE, sep = "\t")
+  }
+  
+  # output in Datei schreiben
+  write.table(output, file=filename, row.names=FALSE, col.names=FALSE, append = TRUE, sep = "\t") 
+  print(paste("Erstellt:",filename,sep = " "))
+  
+}
+
+# Testaufruf mit rma, header und GENENAME:
+print_to_file(Data_All, "rma", TRUE, "GENENAME")
+print_to_file(Data_All, "mas5", TRUE, "SYMBOL")
 
 # Ausgegeben der MMs, PMs und affyIDs:
 # pm(Data_All)
