3fac6331e5494d191cd7adcb83ba99b62aa01b17
emmkey <m.krivan@gmx.de>
2016-04-19 11:08:00 +0200

    Update package_installation.R
    Überprüft vorher, ob Pakete bereits installiert wurden

--- website/R/package_installation.R
+++ website/R/package_installation.R
@@ -1,6 +1,21 @@
 # Installation:
 source("https://bioconductor.org/biocLite.R")
-biocLite("affy")
-biocLite("hgu133plus2.db")
-biocLite("affyQCReport")
-biocLite("affyPLM")
+
+checklib1 <- require(affy,)
+checklib2 <- require(hgu133plus2.db)
+checklib3 <- require(affyQCReport)
+checklib4 <- require(affyPLM)
+
+if (checklib1 != TRUE) {
+	biocLite("affy")
+}
+if (checklib2 != TRUE) {
+	biocLite("hgu133plus2.db")
+}
+if (checklib1 != TRUE) {
+	biocLite("affyQCReport")
+}
+if (checklib1 != TRUE) {
+	biocLite("affyPLM")
+}
+
