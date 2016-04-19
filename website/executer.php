95b9b3d1e2f75fc3b2a08ff7e7ed89893304af9e
ylapawczyk <yannic.lapawczyk@gmail.com>
2016-04-19 19:24:17 +0200

    CEL-Upload => TxT-Datei => DB funktioniert

--- website/executer.php
+++ website/executer.php
@@ -5,14 +5,30 @@ require_once("functions.php");
 
 
 //Verzeichnis fuer den Output erstellen
-if ($outname = makeDir()) {
-	echo "Output Verzeichnis erstellt: $outname";
+if ($dirname = makeDir()) {
+	echo "Output Verzeichnis erstellt: $dirname";
 }
 
-moveCels($outname);
+moveCels($dirname);
 
-//Workingdirectory setzen
-setWd($dirname, $rscript)
+//testscript in WD kopieren
+$file = './R/testscript.R';
+$newfile = "./Output/$dirname/wd/testscript.R";
+
+if (!copy($file, $newfile)) {
+    echo "copy $file schlug fehl...\n";
+}
+
+$file = "./Output/$dirname/wd/testscript.R";
+// Öffnet die Datei, um den vorhandenen Inhalt zu laden
+$current = file_get_contents($file);
+// Fügt eine neue Person zur Datei hinzu
+$setwdR = 'setwd("./Output/' . $dirname . '/wd")' . "\xA";
+$setwdR .= $current;
+
+// Schreibt den Inhalt in die Datei zurück
+file_put_contents($file, $setwdR);
+
 
 
 
@@ -23,18 +39,27 @@ $rpath = $_POST['rpfad'];
 echo "R-Pfad: " . $rpath;
 
 if (strtoupper(substr(php_uname('s'), 0, 3)) === 'WIN') {
+//$command = '"$rpath\bin\R.exe" "C:\XAMPP\htdocs\SWPwebseite\R\testscript.R"';
 $command = '"$rpath\bin\R.exe" "C:\XAMPP\htdocs\SWPwebseite\R\testscript.R"';
 $output = shell_exec($command);
 echo "<pre>$output</pre>";
 }
 else{
-$command = "$rpath/bin/R < /home/admini/Uni/Softwareprojekt/GenEx/website/R/testscript.R --save;";
+//$command = "$rpath/bin/R < /home/admini/Uni/Softwareprojekt/GenEx/website/R/testscript.R --save;";
+$command = "$rpath/bin/R < /home/admini/Uni/Softwareprojekt/GenEx/website/Output/$dirname/wd/testscript.R --save;";
 $output = shell_exec($command);
 echo "<pre>$output</pre>";
 }
 
 //$zitate = file_get_contents(__DIR__.'/output_example.txt');
 //echo "Dateiinhalt: " . nl2br($zitate);
+
+//Durch R-Skript erzeugte Tabelle jetzt in die Datenbank schreiben
+$filename = "./Output/$dirname/tables/outtable.txt";
+
+if ($tablename = writeToDb($filename)) {
+	echo "Tabelle erstellt: $tablename";
+}
 
 
 
--- website/functions.php
+++ website/functions.php
@@ -61,13 +61,19 @@
 	//Verzeichnisse zum Speichern des User-In- und Outputs
 	function makeDir() {
 		$dirname = strval(time());
-		mkdir("./Output/$dirname", 0700);
+
+		$oldumask = umask(0);
+
+		mkdir("./Output/$dirname", 0777);
 		//Ordner fuer Plots
-		mkdir("./Output/$dirname/plots", 0700);
+		mkdir("./Output/$dirname/plots", 0777);
 		//Ordner fuer txt-Tabellen
-		mkdir("./Output/$dirname/tables", 0700);
+		mkdir("./Output/$dirname/tables", 0777);
 		//Ordner erstellen fuer Workingdirectory(R)
-		mkdir("./Output/$dirname/wd", 0700);
+		mkdir("./Output/$dirname/wd", 0777);
+
+		umask($oldumask); 
+
 
 		return $dirname;
 	}
@@ -105,5 +111,117 @@
 		$newscript = $rcode . $rscript;
 
 		return $newscript;
+	}
+
+	//Textdatei in Datenbank schreiben
+	function writeToDb($filename) {
+
+		//zuerst nur die erste Zeile lesen und parsen,
+		//wie viele CEL-Files die Datei hat
+		$myfile = fopen($filename, "r") or die("Unable to open file!");
+		$firstline = fgets($myfile);
+		fclose($myfile);
+
+		//Alle Woerter aus dem Header parsen und in Array $num_colums speichern
+		preg_match_all('/"[^"]*"/', $firstline, $num_colums);
+
+		//Zaehlt, wie viele Spalten nach AffyID u. GENENAME kommen => wie viele CEL-Files die Datei "hat"
+		$cel_counter = count($num_colums[0]) - 2;
+
+		//Logindaten fuer Datenbank setzen
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "celwebsite";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		} 
+
+
+		//Tabellennamen festlegen
+		//Gibt die seit Beginn der Unix-Epoche (Januar 1 1970 00:00:00 GMT) bis jetzt vergangenen Sekunden zurück.
+		//=> einzigartiger Tabellenname
+		//strval: int => string
+		$tablename = strval(time());
+		echo 'Tabellenname: ' . $tablename . "<br>";
+
+		//Tabelle erstellen, die vorerst nur die AffyID als Key enthaelt
+		$sql = "CREATE TABLE `$tablename` (
+		AffyID VARCHAR(30) PRIMARY KEY
+		)";
+
+		if ($conn->query($sql) === TRUE) {
+		    echo "<br> Tabelle $tablename erfolgreich erstellt";
+		} else {
+		    echo "<br> Error bei Tabellenerstellung: " . $conn->error;
+		}
+
+		//Array fuer Header erstellen (wird nach der For-Schleife,
+		//zum Einfuegen der Daten in die Tabelle benoetigt)
+		$header_array = array();
+		$header_array[] = "AffyID";
+
+		//Spalten fuer Info und CEL-Files anfuegen
+		for ($x = 1; $x < sizeof($num_colums[0]); $x++) {
+			$col_name = $num_colums[0][$x];
+			
+			//pruefen, ob .CEL nicht enthalten ist 
+			//dann nur die Anfuehrungszeichen wegschneiden
+			if (strpos($col_name, '.CEL') == false) {
+		    		$col_name = substr($col_name,1,-1);
+			//Wenn .CEL vorhanden, dann Anfuehrungszeichen und ".CEL" wegschneiden	
+			} else {
+				$col_name = substr($col_name,1,-5);
+			}
+			
+			//zurechtgeschnittene Zeilenbezeichnung in header-array schreiben
+			$header_array[] = $col_name;
+
+			$alter_query = "ALTER TABLE `$tablename` ADD `$col_name` VARCHAR(200) NOT NULL";
+			if ($conn->query($alter_query) === TRUE) {
+				echo "<br>" . "Spalte fuer CEL-File hinzugefuegt";
+			} else {
+				echo "<br>" . "Fehler beim Hinzufuegen der CEL-Spalten";
+			}
+		} 
+
+		//Array in String Komma-Separierten String umschreiben
+		$comma_separated = implode(", ",$header_array);
+
+
+
+		    	
+
+		//Jetzt kann die Tabelle gefuellt werden
+
+		$file_to_db = 	"LOAD DATA LOCAL INFILE '" . $filename . "' 
+				INTO TABLE `$tablename` 
+				FIELDS TERMINATED BY '\\t' 
+				OPTIONALLY ENCLOSED BY '\"' 
+				LINES TERMINATED BY '\\n'
+				IGNORE 1 LINES
+				($comma_separated)";
+
+
+		if ($conn->query($file_to_db) === TRUE) {
+			//printf($conn->error);
+			echo "<br> File erfolgreich in Tabelle!!!";	
+
+		} else {
+			printf($conn->error);
+			echo "<br> Fehler beim Einfuegen in Tabelle";
+		}
+
+
+
+
+		$conn->close();
+
+		return $tablename;
+
 	}
 ?>
